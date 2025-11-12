c10394eea1ce4969ac6a57056fbefa7e73169dfc
aluno <aluno@GAS-7B1-919818>
2025-11-12 08:44:01 -0300

    branch01

--- projetoFinalPDS/src/projetoFinalPDS/TelaLogin.java
+++ projetoFinalPDS/src/projetoFinalPDS/TelaLogin.java
@@ -5,11 +5,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class TelaLogin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField tfUsuario;
+	private JTextField tfSenha;
 
 	/**
 	 * Launch the application.
@@ -31,12 +37,38 @@ public class TelaLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaLogin() {
+		setTitle("Tela Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 300, 301);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lbUsuario = new JLabel("Usuário");
+		lbUsuario.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lbUsuario.setBounds(50, 31, 46, 14);
+		contentPane.add(lbUsuario);
+		
+		tfUsuario = new JTextField();
+		tfUsuario.setBounds(106, 29, 121, 20);
+		contentPane.add(tfUsuario);
+		tfUsuario.setColumns(10);
+		
+		tfSenha = new JTextField();
+		tfSenha.setBounds(106, 72, 121, 20);
+		contentPane.add(tfSenha);
+		tfSenha.setColumns(10);
+		
+		JLabel lbSenha = new JLabel("Senha");
+		lbSenha.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lbSenha.setBounds(50, 75, 46, 14);
+		contentPane.add(lbSenha);
+		
+		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton.setBounds(96, 206, 89, 23);
+		contentPane.add(btnNewButton);
 
 	}
-
 }
