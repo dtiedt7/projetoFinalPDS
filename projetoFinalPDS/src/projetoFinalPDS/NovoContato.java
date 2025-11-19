a0f5afd4d0a1dd953dd562ac0f397ef38dba0238
aluno <aluno@GAS-7B1-919818>
2025-11-19 09:03:19 -0300

    com

--- projetoFinalPDS/src/projetoFinalPDS/NovoContato.java
+++ projetoFinalPDS/src/projetoFinalPDS/NovoContato.java
@@ -31,11 +31,13 @@ public class NovoContato extends JFrame {
 	 * Create the frame.
 	 */
 	public NovoContato() {
+		setTitle("Novo Contato");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
 	}
 
